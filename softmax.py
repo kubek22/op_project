eb3532384c779f2f792b2a09defb2cf09e7e3175
Wiktor <103779975+wozniakw2002@users.noreply.github.com>
2025-05-22 20:13:15 +0200

    Tests for dataset from kaggle

---
+++ softmax.py
@@ -0,0 +1,100 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import math
+
+class Net(nn.Module):
+    def __init__(self, input, output):
+        super().__init__()
+        self.soft = nn.Softmax(input, output)
+
+    def forward(self, x):
+        x = self.soft(x)
+        return x
+
+
+def training_epoch(model, dataloader, optimizer, criterion, device):
+    model.train()
+    epoch_loss = 0
+    correct = 0
+    n = 0
+    for x, y in dataloader:
+        x,y = x.to(device), y.to(device)
+        output = model(x)
+        loss = criterion(output, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        _, predicted = output.max(1)
+        correct += predicted.eq(y).sum().item()
+        n += y.size(0)
+
+    avg_loss = epoch_loss / n
+    accuracy = 100 * correct / n
+    return accuracy, avg_loss
+
+
+def evaluate(model,dataloader, criterion, device):
+    total_loss = 0
+    correct = 0
+    n = 0
+    model.eval()
+    with torch.no_grad():
+        for x, y in dataloader:
+            x,y = x.to(device), y.to(device)
+            output = model(x)
+            _, predicted = torch.max(output, 1)
+
+            loss = criterion(output, y)
+            total_loss += loss.item()
+            n += y.size(0)
+            correct += predicted.eq(y).sum().item()
+
+    avg_loss = total_loss / n
+    accuracy = 100 * correct / n
+    return accuracy, avg_loss
+
+
+def train(epochs, model, dataloader_train, dataloader_val, optimizer, criterion, device, model_path,
+        tolerance=math.inf):
+    train_accuracy_list, train_loss_list = [], []
+    val_accuracy_list, val_loss_list = [], []
+    best_loss = float('inf')
+    last_save = 0
+    epochs_without_improvement = 0
+
+    for epoch in range(epochs):
+        train_accuracy, train_avg_loss = training_epoch(model, dataloader_train, optimizer, criterion, device)
+        train_accuracy_list.append(train_accuracy)
+        train_loss_list.append(train_avg_loss)
+        print(f"epoch: {epoch + 1}, training loss: {train_avg_loss}, training accuracy: {train_accuracy}")
+
+        val_accuracy, val_avg_loss = evaluate(model, dataloader_val, criterion, device)
+        val_accuracy_list.append(val_accuracy)
+        val_loss_list.append(val_avg_loss)
+        print(f"epoch: {epoch + 1}, validation loss: {val_avg_loss}, validation accuracy: {val_accuracy}")
+
+        if val_avg_loss < best_loss:
+            best_loss = val_avg_loss
+            torch.save(model.state_dict(), model_path)
+            last_save = epoch + 1
+            epochs_without_improvement = 0
+            print("model saved")
+        else:
+            epochs_without_improvement += 1
+            if epochs_without_improvement > tolerance:
+                print(f"Training stopped. Tolerance {tolerance} exceeded")
+                break
+        print()
+
+    history = {
+        "loss_train": train_loss_list,
+        "accuracy_train": train_accuracy_list,
+        "loss_val": val_loss_list,
+        "accuracy_val": val_accuracy_list,
+        "last_save": last_save
+    }
+    return history
